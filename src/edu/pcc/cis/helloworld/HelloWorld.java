40e4493d5377a3bf529e59863b3841007a46cd72
Henry Stoner <henry.stoner@pcc.edu>
2021-10-23 04:07:03 -0700

    Added javadoc

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,7 +1,22 @@
 package edu.pcc.cis.helloworld;
 import java.util.Date;
 
+/**
+ * Class to print out "Hello World!"
+ *
+ * HS: Created HelloWorld class
+ * HS: imported java.util.Date
+ * HS: added line to print out date in main method
+ *
+ * @author henry Stoner
+ */
+
 public class HelloWorld {
+
+    /**
+     * method main prints out "Hello World!"
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println(new Date());
